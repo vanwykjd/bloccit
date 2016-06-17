c512a5c56c6970010015aff921c07a09d9e1b9c0
Josh Van Wyk <joshvanwyk@ymail.com>
2016-06-17 19:26:07 +0000

    Checkpoint 43 Rails- Voting: completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ActiveRecord::Base
 	belongs_to :topic
 	belongs_to :user
 	has_many :comments, dependent: :destroy
+	has_many :votes, dependent: :destroy
 	has_many :labelings, as: :labelable
     has_many :labels, through: :labelings
 	
@@ -11,4 +12,24 @@ class Post < ActiveRecord::Base
 	validates :body, length: { minimum: 20 }, presence: true
     validates :topic, presence: true
     validates :user, presence: true
+    
+	
+	def up_votes
+		votes.where(value: 1).count
+	end
+	
+	def down_votes
+    	votes.where(value: -1).count
+	end
+	
+	def points
+    	votes.sum(:value)
+	end
+	
+	def update_rank
+    	age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
+    	new_rank = points + age_in_days
+    	update_attribute(:rank, new_rank)
+	end
+
 end

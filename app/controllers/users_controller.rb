ccd197b9f08e2506499b1f6a234fdf7fe5c16a5d
Josh Van Wyk <joshvanwyk@ymail.com>
2016-06-18 08:21:03 +0000

    Checkpoint 45 Rails- Profiles: completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,4 +24,11 @@ class UsersController < ApplicationController
 			
 	end
 	
+	
+	
+	def show
+    	@user = User.find(params[:id])
+    	@posts = @user.posts.visible_to(current_user)
+	end
+	
 end
--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -2,10 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Topic, type: :model do
 	
-	let(:name) { RandomData.random_sentence }
-	let(:description) { RandomData.random_paragraph }
+	
 	let(:public) { true }
-	let(:topic) { Topic.create!(name: name, description: description) }
+	let(:topic) { create(:topic) }
 	
 	it { is_expected.to have_many(:posts) }
 	
@@ -15,7 +14,7 @@ RSpec.describe Topic, type: :model do
 	
 	describe "attributes" do
      it "has name, description, and public attributes" do
-       expect(topic).to have_attributes(name: name, description: description, public: public)
+       expect(topic).to have_attributes(name: topic.name, description: topic.description)
      end
      
      it "is public by default" do

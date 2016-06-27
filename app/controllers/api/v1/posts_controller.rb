b4706034fbfae0ab93ab0eafda1cf0904020ed5f
Josh Van Wyk <joshvanwyk@ymail.com>
2016-06-27 22:02:27 +0000

    Assignment 49 Rails- API Sending Data: completed

---
+++ app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,45 @@
+class Api::V1::PostsController < Api::V1::BaseController
+  
+   before_action :authenticate_user, except: [:index, :show]
+   before_action :authorize_user, except: [:index, :show]
+
+  def update
+    post = Post.find(params[:id])
+
+    if post.update_attributes(post_params)
+      render json: post.to_json, status: 200
+    else
+      render json: {error: "Post updated failed", status: 400}, status: 400
+    end
+  end
+
+  def create
+    topic = Topic.find(params[:topic_id])
+    post = topic.posts.build(post_params)
+    post.user = @current_user
+
+    if post.valid?
+      post.save!
+      render json: post.to_json, status: 200
+    else
+      render json: { error: 'Post is invalid', status: 400 }, status: 400
+    end
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+
+    if post.destroy
+      render json: { message: 'Post destroyed', status: 200 }, status: 200
+    else
+      render json: { error: 'Post destoy failed', status: 400 }, status: 400
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -25,7 +25,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:index, :show, :create, :update]
-      resources :topics, except: [:edit, :new]
+       resources :topics, except: [:edit, :new ] do
+        resources :posts, except: [:index]
+      end
     end
   end
 
